1b5f7aa6e66b4322be2e6333420a78f959abd7c7
Nicholos Nauth <77813014+nicholosnauth@users.noreply.github.com>
2023-10-27 23:00:07 -0400

    Hero.jsx file created
    Front page styling using Tailwind is used. The basic structure is started but no css is working yet.

--- photography-portfolio/pages/index.js
+++ photography-portfolio/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Hero from '../components/Hero';
 
 
 export default function Home() {
@@ -8,8 +9,13 @@ export default function Home() {
         <title>Photography Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Hero/>
+
 
      
     </div>
   );
 }
+
+
+
