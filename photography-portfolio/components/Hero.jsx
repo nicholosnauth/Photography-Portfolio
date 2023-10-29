1aea105f607abf1554eaa457ff31cbf199401e9d
Nicholos Nauth <77813014+nicholosnauth@users.noreply.github.com>
2023-10-28 23:28:50 -0400

    minor changes

--- photography-portfolio/components/Hero.jsx
+++ photography-portfolio/components/Hero.jsx
@@ -1,8 +1,8 @@
-import React from "react"
+import React from "react";
 
 const Hero = () => {
- return (
-       <div className='flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img'>
+    return (
+       <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img">
         {/* Overlay */}
         <div/>
         <div>
--- photography-portfolio/pages/index.js
+++ photography-portfolio/pages/index.js
@@ -1,5 +1,8 @@
 import Head from 'next/head';
+import './styles/globals.css'
 import Hero from '../components/Hero';
+
+
 
 
 export default function Home() {
@@ -9,10 +12,7 @@ export default function Home() {
         <title>Photography Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Hero/>
-
-
-     
+      <Hero/>   
     </div>
   );
 }
