b9efc2926d13b9583fd8fc696cb9ebc91683bdf9
Nicholos Nauth <77813014+nicholosnauth@users.noreply.github.com>
2023-11-08 18:18:55 -0500

    contact page completed

--- photography-portfolio/pages/contact.jsx
+++ photography-portfolio/pages/contact.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import Contact from '../components/Contact'
 
 const contact = () => {
   return (
-    <div>contact</div>
+    <div>
+    <Hero  heading='Contact'/>
+    <Contact />
+    </div>
   )
 }
 
